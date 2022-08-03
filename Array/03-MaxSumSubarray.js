8510334581adc5cf8dc2a843765775111ac232d0
Shubh <shubh410shekhar@gmail.com>
2022-08-03 17:45:13 +0530

    MaxSumSubarray

---
+++ Array/03-MaxSumSubarray.js
@@ -0,0 +1,20 @@
+function printingSubArray(arr) {
+    let maxSum=arr[0];
+    for(let i =0; i<arr.length; i++){
+        for(let j = i; j<arr.length; j++) {
+            let arrNew = [];
+            let sum = 0;
+            for(let k = i; k<=j; k++) {
+                arrNew.push(arr[k]);
+                sum += arr[k];
+            }
+            if(sum>maxSum){
+                maxSum = sum;
+            }
+        }
+    }
+    console.log(maxSum);
+
+}
+
+printingSubArray([5,2,-4,-5, 3,-1,2,3,1])
